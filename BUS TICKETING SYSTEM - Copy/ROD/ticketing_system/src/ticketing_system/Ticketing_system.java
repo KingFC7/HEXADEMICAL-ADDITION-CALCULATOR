efce4ae5e8b74eeef38d79a83b3fb208cf4bc51f
KingFC7 <117495903+KingFC7@users.noreply.github.com>
2024-01-05 16:13:30 +0800

    Delete BUS TICKETING SYSTEM - Copy/ROD directory

--- BUS TICKETING SYSTEM - Copy/ROD/ticketing_system/src/ticketing_system/Ticketing_system.java
+++
@@ -1,9 +0,0 @@
-package ticketing_system;
-public class Ticketing_system {
-    public static void main(String[] args) {
-        login portal = new login();
-        portal.setVisible(true);
-        portal.setLocationRelativeTo(null);
-    }
-    
-}
